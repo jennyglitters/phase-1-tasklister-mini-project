bca644e5cde380d92d459c424363a5d6dcd7f223
jennipher <voliduser@gmail.com>
2023-08-03 11:25:45 -0400

    mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+let task = document.getElementById('task');
+let submit = document.getElementById('create-task-form');
+let taskBar = document.getElementById('new-task-description');
+let uList = document.getElementById('list');
+
+submit.addEventListener('submit',(e)=>{
+  e.preventDefault();
+let newTask = taskBar.value;
+let taskList = document.createElement('li');
+taskList.textContent = newTask;
+uList.appendChild(taskList);
+taskDescription.value= '';
+})
